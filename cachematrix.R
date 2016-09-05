a73ccc70915ea2f502fd8f83753340ec7f5fbefb
InspectorButters <bob.herget@gmail.com>
2016-09-04 17:46:24 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##Overview:  The purpose of this code is to (1) calculate the inverse of a matrix and
+## (2) to cache and recall this inverse matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
+##set value of myMatrix
+myMatrix <- matrix(c(1,2,3,5,7,11,13,17,19), 3, 3)
 
+##this function inverts myMatrix
+myMatrixInvert <- function(myMatrix){
+  solve(myMatrix)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##the following function is supposed to cache the matrix
+##inputted above in myMatrix
+makeCacheMatrix <- function(x = myMatrix()) {
+  m <- NULL
+  setMatrix <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  getMatrix <- function() x
+  setInverse <- function(solve(x)) m <<- solve(x)
+  getInverse <- function() m
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
+
